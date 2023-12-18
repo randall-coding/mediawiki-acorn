620b3c8ddc83f5bf68f5d742ff148dda3ecb5297
Randall Coding <randallcoding@protonmail.com>
2023-12-18 12:03:19 +0700

    Adds secret file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 acorn build -t ghcr.io/randall-coding/acorn/mediawiki && \
 acorn push ghcr.io/randall-coding/acorn/mediawiki && \
-acorn run -n mediawiki --memory=2Gi ghcr.io/randall-coding/acorn/mediawiki
+acorn run -s mediawiki:mediawiki -n mediawiki --memory=1Gi ghcr.io/randall-coding/acorn/mediawiki
