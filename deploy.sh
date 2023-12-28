b4d00b1021814b98a732aad27a86b60cb88580f9
Randall Coding <randallcoding@protonmail.com>
2023-12-28 16:03:58 +0700

    Updates deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 acorn build -t ghcr.io/randall-coding/acorn/mediawiki && \
 acorn push ghcr.io/randall-coding/acorn/mediawiki && \
-acorn run -s mediawiki:mediawiki -n mediawiki --memory=1Gi ghcr.io/randall-coding/acorn/mediawiki
+acorn run -s mediawiki-mariadb:mediawiki-mariadb -n mediawiki --memory=1Gi ghcr.io/randall-coding/acorn/mediawiki
